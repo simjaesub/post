769b29af240f490333e90043dd54d741bee124bf
simjaesub <simjaesub23@gmail.com>
2020-02-12 15:30:49 +0100

    channge back to 600dpi

--- Shift_of_cage.py
+++ Shift_of_cage.py
@@ -257,7 +257,7 @@ plt.plot(sFCV2x, sFCV2y, 'firebrick', alpha=0.5)
 
 
 plt.tight_layout()
-plt.savefig('.\cage_shape_figure\cage_shift.png', dpi=300)
+plt.savefig('.\cage_shape_figure\cage_shift.png', dpi=600)
 
 ############################# lengths - Initial #################################
 
