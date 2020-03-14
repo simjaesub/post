c5d7be295c9cd2063fe07f39a52f76d675b49387
simjaesub <simjaesub23@gmail.com>
2020-03-14 02:04:53 +0100

    single_cage 3d plot_intact model

---
+++ 3d_singlecage_plot/3d_singlecage_deformedstate.py
@@ -0,0 +1,156 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import magicFun as mf
+
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["figure.figsize"] = (18.3, 9.5)
+plt.rcParams['lines.linewidth'] = 1
+plt.rcParams['font.size'] = '8'
+plt.rcParams["mathtext.default"] = "it"
+plt.rcParams["mathtext.fontset"] = "stix"
+import pickle
+
+with open('..\\..\Results_singlecage_intact_d\\dprocessed_Mul1x1Vel0.5Degree0.csv', 'rb') as handle:
+    resui = pickle.load(handle)
+
+############
+# Enter Time#
+
+time = 0
+############
+
+## Anchor positions : U anchors and V anchors in turn
+anchor_pos = [[-150, -50, 80],
+              [-150, 50, 80],
+              [150, -50, 80],
+              [150, 50, 80],
+              [-50, -150, 80],
+              [50, -150, 80],
+              [-50, 150, 80],
+              [50, 150, 80]
+              ]
+## Buoy positions
+buoy = []
+for i in range(0, 2):
+    for j in range(0, 2):
+        buoy.append(resui['mooring3']['platePosition' + str(i) + '_' + str(j)][time, 0:3])
+
+'''
+#################
+  Initial state
+#################
+'''
+
+fig = plt.figure()
+ax1 = fig.gca(projection='3d')
+plt.gca().invert_yaxis()
+ax1.invert_zaxis()
+##################### mooring lines ##################################
+moor_Ux = []
+moor_Uy = []
+moor_Uz = []
+moor_Vx = []
+moor_Vy = []
+moor_Vz = []
+
+# U anchors
+for i in range(0, 4):
+    moor_Ux.append([anchor_pos[i][0], buoy[i][0]])
+    moor_Uy.append([anchor_pos[i][1], buoy[i][1]])
+    moor_Uz.append([anchor_pos[i][2], buoy[i][2]])
+# V anchors
+adjust = [0, 2, 1 / 2, 1]
+for i in range(0, 4):
+    moor_Vx.append([anchor_pos[i + 4][0], buoy[int(adjust[i] * i)][0]])
+    moor_Vy.append([anchor_pos[i + 4][1], buoy[int(adjust[i] * i)][1]])
+    moor_Vz.append([anchor_pos[i + 4][2], buoy[int(adjust[i] * i)][2]])
+
+for i in range(0, 4):
+    plt.plot(moor_Ux[i], moor_Uy[i], moor_Uz[i], 'k')
+for i in range(0, 4):
+    plt.plot(moor_Vx[i], moor_Vy[i], moor_Vz[i], 'k')
+
+################# Frame cables ######################################
+FCUx = []
+FCUy = []
+FCUz = []
+upper = [0]
+lower = [1]
+for i in upper:
+    FCUx.append([buoy[i][0], buoy[i + 2][0]])
+    FCUy.append([buoy[i][1], buoy[i + 2][1]])
+    FCUz.append([buoy[i][2], buoy[i + 2][2]])
+for i in lower:
+    FCUx.append([buoy[i][0], buoy[i + 2][0]])
+    FCUy.append([buoy[i][1], buoy[i + 2][1]])
+    FCUz.append([buoy[i][2], buoy[i + 2][2]])
+
+FCVx = []
+FCVy = []
+FCVz = []
+for i in range(0, 4, 2):
+    FCVx.append([buoy[i][0], buoy[i + 1][0]])
+    FCVy.append([buoy[i][1], buoy[i + 1][1]])
+    FCVz.append([buoy[i][2], buoy[i + 1][2]])
+for i in range(0, 2):
+    plt.plot(FCUx[i], FCUy[i], FCUz[i], 'g')
+for i in range(0, 2):
+    plt.plot(FCVx[i], FCVy[i], FCVz[i], 'g')
+
+## netting
+pos_i = np.zeros((321, 3))
+for i in range(1, 322):
+    pos_i[i - 1][0] = resui['NetStructure0']['Pos_' + str(i)][time, 0]
+    pos_i[i - 1][1] = resui['NetStructure0']['Pos_' + str(i)][time, 1]
+    pos_i[i - 1][2] = resui['NetStructure0']['Pos_' + str(i)][time, 2]
+
+mf.drawCageV(pos_i, ax1)
+mf.drawCageH(pos_i, ax1)
+
+##### Bridles
+
+
+for cage in range(0, 1):
+    ### Bridle 1-3 ###
+    for i in range(1, 4):
+        bx = [buoy[3 + 2 * cage][0], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 0]]
+        by = [buoy[3 + 2 * cage][1], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 1]]
+        bz = [buoy[3 + 2 * cage][2], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 2]]
+        ax1.plot(bx, by, bz, color='firebrick')
+
+    ### Bridle 4-6 ###
+    for i in range(4, 7):
+        bx = [buoy[1 + 2 * cage][0], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 0]]
+        by = [buoy[1 + 2 * cage][1], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 1]]
+        bz = [buoy[1 + 2 * cage][2], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 2]]
+        ax1.plot(bx, by, bz, color='firebrick')
+
+    ### Bridle 7-9 ###
+    for i in range(7, 10):
+        bx = [buoy[0 + 2 * cage][0], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 0]]
+        by = [buoy[0 + 2 * cage][1], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 1]]
+        bz = [buoy[0 + 2 * cage][2], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 2]]
+        ax1.plot(bx, by, bz, color='firebrick')
+
+    ### Bridle 10-12 ###
+    for i in range(10, 13):
+        bx = [buoy[2 + 2 * cage][0], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 0]]
+        by = [buoy[2 + 2 * cage][1], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 1]]
+        bz = [buoy[2 + 2 * cage][2], resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 2]]
+        ax1.plot(bx, by, bz, color='firebrick')
+    Con_pointsX = []
+    Con_pointsY = []
+    Con_pointsZ = []
+
+    for i in range(1, 13):
+        Con_pointsX = np.append(Con_pointsX, resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 0])
+        Con_pointsY = np.append(Con_pointsY, resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 1])
+        Con_pointsZ = np.append(Con_pointsZ, resui['collar' + str(cage)]['FastConnectPos' + str(i)][time, 2])
+    Con_pointsX = np.append(Con_pointsX, Con_pointsX[0])
+    Con_pointsY = np.append(Con_pointsY, Con_pointsY[0])
+    Con_pointsZ = np.append(Con_pointsZ, Con_pointsZ[0])
+
+    ax1.plot(Con_pointsX, Con_pointsY, Con_pointsZ, 'k', linewidth=5)
+
+plt.axis('off')
+ax1.view_init(elev=50, azim=90)
---
+++ 3d_singlecage_plot/magicFun.py
@@ -0,0 +1,69 @@
+cage = [
+    [1, 2, 5, 7, 9, 51, 53, 55, 57, 91, 93, 95, 97, 131, 133, 135, 137, 171, 173, 175, 177, 211, 213, 215, 217, 251,
+     253, 255, 257, 291, 293, 295],
+    [4, 3, 6, 8, 10, 52, 54, 56, 58, 92, 94, 96, 98, 132, 134, 136, 138, 172, 174, 176, 178, 212, 214, 216, 218, 252,
+     254, 256, 258, 292, 294, 296],
+    [12, 11, 13, 14, 15, 59, 60, 61, 62, 99, 100, 101, 102, 139, 140, 141, 142, 179, 180, 181, 182, 219, 220, 221, 222,
+     259, 260, 261, 262, 297, 298, 299],
+    [17, 16, 18, 19, 20, 63, 64, 65, 66, 103, 104, 105, 106, 143, 144, 145, 146, 183, 184, 185, 186, 223, 224, 225, 226,
+     263, 264, 265, 266, 300, 301, 302],
+    [22, 21, 23, 24, 25, 67, 68, 69, 70, 107, 108, 109, 110, 147, 148, 149, 150, 187, 188, 189, 190, 227, 228, 229, 230,
+     267, 268, 269, 270, 303, 304, 305],
+    [27, 26, 28, 29, 30, 71, 72, 73, 74, 111, 112, 113, 114, 151, 152, 153, 154, 191, 192, 193, 194, 231, 232, 233, 234,
+     271, 272, 273, 274, 306, 307, 308],
+    [32, 31, 39, 43, 47, 75, 79, 83, 87, 115, 119, 123, 127, 155, 159, 163, 167, 195, 199, 203, 207, 235, 239, 243, 247,
+     275, 279, 283, 287, 309, 313, 317],
+    [34, 33, 40, 44, 48, 76, 80, 84, 88, 116, 120, 124, 128, 156, 160, 164, 168, 196, 200, 204, 208, 236, 240, 244, 248,
+     276, 280, 284, 288, 310, 314, 318],
+    [36, 35, 41, 45, 49, 77, 81, 85, 89, 117, 121, 125, 129, 157, 161, 165, 169, 197, 201, 205, 209, 237, 241, 245, 249,
+     277, 281, 285, 289, 311, 315, 319],
+    [38, 37, 42, 46, 50, 78, 82, 86, 90, 118, 122, 126, 130, 158, 162, 166, 170, 198, 202, 206, 210, 238, 242, 246, 250,
+     278, 282, 286, 290, 312, 316, 320],
+]
+
+
+def drawCageH(nodePosition, handle):
+    '''
+    :param nodePosition:  a n*3 numpy array. n is the number of node.
+    :param handle: the handle to plot the figure.
+    :return: plot the horizontal lines.
+    '''
+    xs = []
+    ys = []
+    zs = []
+    for i in range(0, 10):
+        for j in range(0, 32):
+            xs.append(nodePosition[cage[i][j]][0])
+            ys.append(nodePosition[cage[i][j]][1])
+            zs.append(nodePosition[cage[i][j]][2])
+        xs.append(nodePosition[cage[i][0]][0])
+        ys.append(nodePosition[cage[i][0]][1])
+        zs.append(nodePosition[cage[i][0]][2])
+        handle.plot(xs, ys, zs, color='blue')
+        xs = []
+        ys = []
+        zs = []
+    handle.scatter(nodePosition[0][0], nodePosition[0][1], nodePosition[0][2], color='blue')
+
+
+def drawCageV(nodePosition, handle):
+    '''
+    :param nodePosition: a n*3 numpy array. n is the number of node.
+    :param handle: the handle to plot the figure.
+    :return: plot the vertical lines.
+    '''
+    xs = []
+    ys = []
+    zs = []
+    for j in range(0, 32):
+        for i in range(0, 10):
+            xs.append(nodePosition[cage[i][j]][0])
+            ys.append(nodePosition[cage[i][j]][1])
+            zs.append(nodePosition[cage[i][j]][2])
+        xs.append(nodePosition[0][0])
+        ys.append(nodePosition[0][1])
+        zs.append(nodePosition[0][2])
+        handle.plot(xs, ys, zs, color='blue')
+        xs = []
+        ys = []
+        zs = []
